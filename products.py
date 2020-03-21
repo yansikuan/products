c2384a1674bd54433d2f745cb74fd19019bdc210
Sikuan Yan <yansikuan@gmail.com>
2020-03-21 18:31:38 +0100

    first commit

--- products.py
+++ products.py
@@ -10,3 +10,11 @@ while True:
 	# equiv to p = [name,price]
 	products.append(p)
 print(products)
+
+for p in products:
+	print('Price for', p[0], 'is',p[1])
+
+with open('Products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品,价钱\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
